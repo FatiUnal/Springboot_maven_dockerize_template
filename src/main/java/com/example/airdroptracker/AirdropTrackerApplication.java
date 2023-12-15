ed89203c23eec74aa0478328f938164fa17ba8a9
Fatih <fatih_0226@outlook.com>
2023-12-15 12:13:25 +0300

    first commit

---
+++ src/main/java/com/example/airdroptracker/AirdropTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.airdroptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdropTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirdropTrackerApplication.class, args);
+    }
+
+}
